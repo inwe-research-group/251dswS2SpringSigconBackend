2c943433514ec4527500e03cc07e11b21198db75
inwe-research-group <zmamanir@unmsm.edu.pe>
2025-04-02 21:44:42 -0500

    add postMapping insertPersona

--- src/main/java/dsw/sigconbackend/controller/PersonaController.java
+++ src/main/java/dsw/sigconbackend/controller/PersonaController.java
@@ -1,5 +1,6 @@
 package dsw.sigconbackend.controller;
 
+import dsw.sigconbackend.dto.PersonaRequest;
 import dsw.sigconbackend.dto.PersonaResponse;
 import dsw.sigconbackend.service.PersonaService;
 import dsw.sigconbackend.utils.ErrorResponse;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +37,19 @@ public class PersonaController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.builder().message("Persona not found").build());
         return ResponseEntity.ok(listaPersonaResponse);        
     }
-    
+    @PostMapping()
+    public ResponseEntity<?> insertPersona(@RequestBody PersonaRequest personaRequest){
+        logger.info(">insert " + personaRequest.toString());
+        PersonaResponse personaResponse;
+        try{
+            personaResponse=personaService.insertPersona(personaRequest);
+            
+        }catch(Exception e){
+            logger.error("Error inesperado",e);
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if(personaResponse==null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.builder().message("Persona not insert").build());
+        return ResponseEntity.ok(personaResponse);                
+    }
 }
--- src/main/java/dsw/sigconbackend/service/PersonaService.java
+++ src/main/java/dsw/sigconbackend/service/PersonaService.java
@@ -1,6 +1,10 @@
 package dsw.sigconbackend.service;
 
+import dsw.sigconbackend.dto.PersonaRequest;
 import dsw.sigconbackend.dto.PersonaResponse;
+import dsw.sigconbackend.model.Persona;
+import dsw.sigconbackend.model.TipoDocumento;
+import dsw.sigconbackend.model.Ubigeo;
 import dsw.sigconbackend.repository.PersonaRepository;
 import dsw.sigconbackend.repository.TipoDocumentoRepository;
 import dsw.sigconbackend.repository.UbigeoRepository;
@@ -19,6 +23,30 @@ public class PersonaService {
     
     public List<PersonaResponse> listPersonas(){
         return PersonaResponse.fromEntities(personaRepository.findAll());
+    }
+    
+    public PersonaResponse insertPersona(PersonaRequest personaRequest){
+        Integer idTipoDocumento = personaRequest.getIdTipoDocumento();
+        TipoDocumento tipoDocumento =tipoDocumentoRepository.findById(idTipoDocumento).get();
+        if(tipoDocumento==null) return new PersonaResponse();
+        
+        String idUbigeo=personaRequest.getIdUbigeo();
+        Ubigeo ubigeo=ubigeoRepository.findById(idUbigeo).get();
+        if(ubigeo==null) return new PersonaResponse();
+        
+        Persona persona=new Persona(
+            personaRequest.getIdPersona(),
+            personaRequest.getApellidoPaterno(),
+            personaRequest.getApellidoMaterno(),
+            personaRequest.getNombres(),
+            personaRequest.getFechaNacimiento(),
+            personaRequest.getNDocumento(),
+            personaRequest.getDireccion(),
+            tipoDocumento,
+            ubigeo
+        );        
+        persona=personaRepository.save(persona);
+        return PersonaResponse.fromEntity(persona);
     }
     
 }
